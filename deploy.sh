dc67a1aad1df12707b5e6d4a461d86c6daf5fd0f
tomasdelima <tomasdelima@gmail.com>
2015-01-16 00:52:04 -0200

    Estiliza um pouco algumas views

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ cd ~/Projects/prayers
 adb tcpip 5555
 adb connect 192.168.0.184
 
-if ionic run --livereload --device android
+if ionic run --device android #--livereload
   then echo 'deployed'
 fi
