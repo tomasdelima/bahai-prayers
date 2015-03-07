bf5a06b6f2484f488943cad97b1ecef7c65a636c
tomasdelima <tomasdelima@gmail.com>
2015-03-07 02:16:58 -0300

    Adiciona campos ativo nas categorias e orações

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,7 @@
-var services = angular.module('services', []),
-    verbose = false 
+services = angular.module('services', [])
+versionDate = Number(new Date(2015, 3-1, 5))
+verbose = false
+// verbose = true
 
 services.service('CategoriesService', function($http, DBService) {
   var self = this
@@ -19,11 +21,13 @@ services.service('CategoriesService', function($http, DBService) {
 
         DBService.loadFromRemoteServer(url+'/categories', self.categories, 'lastUpdatedCategoriesAt', function(data){
           data.forEach(function(d){
-            if(existingIds.indexOf(Number(d.id)) == -1) {
-              DBService.insert('categories_table', ['id', 'title'], [d.id, d.title])
+            if (existingIds.indexOf(Number(d.id)) == -1) {
+              DBService.insert('categories_table', [ 'id', 'title', 'active'],
+                                                   [d.id, d.title, d.active ])
               self.categories.push(d)
             } else {
-              DBService.update('categories_table', ['title'], [d.title], d.id)
+              DBService.update('categories_table', [ 'title', 'active'],
+                                                   [d.title, d.active ], d.id)
             }
           })
         })
@@ -50,11 +54,13 @@ services.service('PrayersService', function($http, DBService) {
 
         DBService.loadFromRemoteServer(url+'/prayers', self.categories, 'lastUpdatedPrayersAt', function(data){
           data.forEach(function(d){
-            if(existingIds.indexOf(Number(d.id)) == -1) {
-              DBService.insert('prayers_table', ['id', 'preamble', 'body', 'author', 'categoryId'], [d.id, d.preamble, d.body, d.author, d.category_id])
+            if (existingIds.indexOf(Number(d.id)) == -1) {
+              DBService.insert('prayers_table', [ 'id', 'preamble', 'body', 'author', 'categoryId',  'active'],
+                                                [d.id, d.preamble, d.body, d.author, d.category_id, d.active])
               self.prayers.push(d)
             } else {
-              DBService.update('prayers_table', ['preamble', 'body', 'author', 'categoryId'], [d.preamble, d.body, d.author, d.category_id], d.id)
+              DBService.update('prayers_table', [ 'preamble', 'body', 'author', 'categoryId', 'active' ],
+                                                [d.preamble, d.body, d.author, d.category_id, d.active], d.id)
             }
           })
         })
@@ -69,17 +75,12 @@ services.service('PrayersService', function($http, DBService) {
 services.service('DBService', function($http){
   return {
     load: function() {
-      log('Preparing DB')
+      db = this
+      log('>> Preparing DB')
       angular.db = openDatabase('bahai-prayers', '1.0', 'bahai-prayers-db', 2 * 1024 * 1024)
-      angular.db.transaction(function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS prayers_table (id integer primary key, categoryId integer, body text, author text)')
-        tx.executeSql('CREATE TABLE IF NOT EXISTS categories_table (id integer primary key, title text)')
-        if(localStorage.lastUpdatedDBSchemaAt < 14242959922100 ) {
-          tx.executeSql('ALTER TABLE prayers_table ADD COLUMN preamble text')
-          log('Added new column to prayers_table: preamble')
-        }
-      })
-      localStorage.lastUpdatedDBSchemaAt = Date.now()
+      if (localStorage.lastUpdatedDBSchemaAt < versionDate) {
+        db.recreateDB()
+      }
     },
     loadFromRemoteServer: function(url, collection, lastUpdatedVariable, callBack) {
       var self = this,
@@ -97,10 +98,10 @@ services.service('DBService', function($http){
           errors += 1
         })
 
-      if(!errors) { localStorage[lastUpdatedVariable] = Date.now() }
+      if (!errors) { localStorage[lastUpdatedVariable] = Date.now() }
     },
     select: function(table, collection, id){
-      var sqlString = 'select * from ' + table + (id ? ' where id = "' + id + '"' : '')
+      var sqlString = 'select * from ' + table + ' where active="true" ' + (id?'and id="'+id+'"':'')
       this.execute(sqlString, function(results) {
         for (var i=0; i<results.rows.length; i++) {
           collection.push(results.rows.item(i))
@@ -131,20 +132,36 @@ services.service('DBService', function($http){
         tx.executeSql(sqlString,[],
           function(tx, results) {
             log('Query executed successfully' + (verbose ? ': ' + sqlString : ''))
-            if(callBack){callBack(results)}
+            if (callBack){callBack(results)}
           },
           function(tx, error) {
-            log('Error executing query' + error.message)
+            log('Error executing query: ' + error.message)
           }
         )
       })
+    },
+    executeAndLog: function(sqlString) {
+      db.execute(sqlString, function(r){for(i=0;i<r.rows.length;i++){log(r.rows.item(i))}})
     },
     resetDB: function(){
       this.delete('categories_table')
       this.delete('prayers_table')
       localStorage.lastUpdatedCategoriesAt = 0
       localStorage.lastUpdatedPrayersAt = 0
-      localStorage.lastUpdatedDBSchemaAt = 0
+    },
+    recreateDB: function() {
+      log('>>>> Recreating DB')
+      db.execute('DROP TABLE prayers_table', function(){
+        db.execute('CREATE TABLE prayers_table ( id integer primary key, categoryId integer, body text, author text, preamble text, favorite boolean, active boolean )')
+      })
+      db.execute('DROP TABLE categories_table', function(){
+        db.execute('CREATE TABLE categories_table ( id integer primary key, title text, active boolean )')
+      })
+      log('>>>> DB recreated successfully')
+
+      localStorage.lastUpdatedCategoriesAt = 0
+      localStorage.lastUpdatedPrayersAt = 0
+      localStorage.lastUpdatedDBSchemaAt = Date.now()
     }
   }
 })
