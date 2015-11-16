6794b2d3449a0088f7d32f9b6b4f2bd32da464a7
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2015-11-16 09:31:00 -0200

    Altera alguns estilos

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 source ~/.bash_profile
-cd ~/Projects/prayers
+cd ~/projetos/bahai-prayers
 
 # adb tcpip 5555
 # adb connect 192.168.0.184
 
-if ionic run --device android
-  then echo 'deployed'
-fi
+ionic build android --release
 
 # # Added key to deploy
-# keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 
 # # Signed APK
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk alias_name
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk alias_name
 
 # # Zipalign the APK
-# ~/Custom\ programs/android-sdk-linux/build-tools/20.0.0/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/BahaiPrayers.apk
+~/Custom\ programs/android-sdk-linux/build-tools/20.0.0/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk platforms/android/ant-build/BahaiPrayers.apk
