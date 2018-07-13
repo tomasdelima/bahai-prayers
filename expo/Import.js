8a847661f98d76201b3e697229f9bc49fd5a7c2a
Tomás Düringer Jacques de Lima <tomasdelima@gmail.com>
2018-07-13 17:33:24 -0300

    Renaming

--- expo/Import.js
+++ expo/Import.js
@@ -3,15 +3,13 @@ global.Axios = require('axios').default
 import ReactNative from 'react-native';
 ["AsyncStorage", "StyleSheet", "Text", "TouchableHighlight", "TouchableOpacity", "View", "ScrollView", "Image"].map(i => global[i] = ReactNative[i])
 
-global.Config    = require('./Config').default
-global.ApiClient = require('./ApiClient').default
+global.Config    = require('./js/Config').default
+global.ApiClient = require('./js/ApiClient').default
+global.Store     = require('./js/Store').default
 
-global.Store          = require('./js/Store').default
-global.LanguageSelect = require('./js/LanguageSelect').default
-global.Flag           = require('./js/Flag').default
-
-global.Tags = require('./components/Tags').default
-global.Tag  = require('./components/Tag').default
+global.LanguageSelect = require('./components/LanguageSelect').default
+global.Tags           = require('./components/Tags').default
+global.Tag            = require('./components/Tag').default
 
 global.s    = require('./lib/Styles').default
 global.Flex = require('./lib/Flex').default
