7dc3a191ffc1cbd484304c9e52f4a9db27a50db9
Tomás Lima <tomasdelima@gmail.com>
2020-01-20 13:53:16 -0300

    Reinitiating the expo folder

---
+++ expo/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import './Import'
+
+export default class App extends React.Component {
+  render () {
+    return <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  }
+}
